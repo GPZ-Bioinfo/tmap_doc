de511f03c7e1a60413c1a702dced10b5fd764945
liaoth <l0404th@gmail.com>
2018-08-07 13:56:45 +0800

    fix env

--- conf.py
+++ conf.py
@@ -201,10 +201,3 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
-
-
-import pip
-
-def install(package):
-    pip.main(['install', package])
-install('git+git://github.com/444thLiao/tmap')
